756ca5d9d1440f5eaee6beb255eb77e489417221
yongtao zhai <501671900@qq.com>
2020-05-12 00:04:47 +0800

    feat： 增加 enum 样例代码

--- 2_TypeScriptEnum/demo.ts
+++ 2_TypeScriptEnum/demo.ts
@@ -1,18 +1,52 @@
 enum Status {
-    OFFLINE,
-    ONLINE,
-    DELETED
+  OFFLINE,
+  ONLINE,
+  DELETED,
+}
+// 枚举的默认值是从0 开始
+// enum Status {
+//     OFFLINE = 0,
+//     ONLINE = 1,
+//     DELETED = 2
+// }
+
+enum Status2 {
+  OFFLINE,
+  ONLINE = 4,
+  DELETED,
 }
 
-function test (x:number):void{
-    if(Status.OFFLINE ==x) {
-        console.info('OFFLINE')
-    } 
-    if(Status.ONLINE ==x) {
-        console.info('ONLINE')
-    } 
-    if(Status.DELETED ==x) {
-        console.info('DELETED')
-    } 
+enum Status3 {
+  OFFLINE = "OFFLINE",
+  ONLINE = "ONLINE",
+  DELETED = "DELETED",
 }
-test
+
+function test(x: number): void {
+  if (Status.OFFLINE == x) {
+    console.info("OFFLINE");
+  }
+  if (Status.ONLINE == x) {
+    console.info("ONLINE");
+  }
+  if (Status.DELETED == x) {
+    console.info("DELETED");
+  }
+}
+test(1);
+
+function output(x) {
+  console.info("x", x);
+}
+output(Status2.OFFLINE); //x 0
+output(Status2.ONLINE); //x 4
+output(Status2.DELETED); //x 5
+
+output(Status2[0]); //x OFFLINE
+output(Status2[4]); //x ONLINE
+output(Status2[5]); //x DELETED
+
+
+output(Status3.OFFLINE); //x OFFLINE
+output(Status3.ONLINE); //x ONLINE
+output(Status3.DELETED); //x DELETED
