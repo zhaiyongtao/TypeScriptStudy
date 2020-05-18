1c183057fa73a76a063cff153c230fc6c8751201
yongtao zhai <501671900@qq.com>
2020-05-19 00:06:18 +0800

    feat:增加泛型在类中的代码

---
+++ 4_4_TypeScriptClassAndGeneric/demo.ts
@@ -0,0 +1,33 @@
+// interface Item {
+//   name: string;
+// }
+
+// class DataManager<T extends number | string> {
+//   constructor(private data: T[]) {}
+//   getItem(index: number): T {
+//     return this.data[index];
+//   }
+// }
+
+// const data = new DataManager<number>([1]);
+// data.getItem(0);
+
+// const data = new DataManager([
+//   {
+//     name: 'dell'
+//   }
+// ]);
+
+// interface Test {
+//   name: string;
+// }
+
+// const data = new DataManager<number>([]);
+
+// 如何使用泛型作为一个具体的类型注解
+function hello1<T>(params: T) {
+    return params;
+  }
+  
+  const func1: <T>(param: T) => T = hello1;
+  
