a34c240b8b9d1453b16cbe4f8e26ac1dc0798e83
zhaiyongtao <501671900@qq.com>
2020-07-10 17:31:17 +0800

    fix：增加文件类的装饰器2

--- 5_2_TypeScriptDecorator2/src/index.ts
+++ 5_2_TypeScriptDecorator2/src/index.ts
@@ -6,3 +6,62 @@
  * @Copyright: Copyright (c) 2020, Hand
  */
 
+// 装饰器工厂
+// 重载构造函数
+// 类装饰器表达式会在运行时当作函数被调用，类的构造函数作为其唯一的参数；
+// 如果类装饰器返回一个值，它会使用提供的构造函数来替换类的声明；
+
+function testDecorator() {
+  return  function <T extends new (...args: any[]) => any>(constructor: T) {
+      //  把类的构造函数作为参数传入，并修改构造函数
+        return class extends constructor {
+            // 修改当前类的构造函数
+            name = 'zyt'
+            getName() {
+                return this.name;
+            }
+        }
+    }
+}
+const Test = testDecorator () (
+    class  {
+        name: string;
+        constructor(name: string) {
+            this.name = name
+        }
+    }
+)
+
+const test = new Test('zyt')
+test.getName();
+console.log(test.getName())
+
+
+function testDecorator2() {
+    return  function <T extends new (...args: any[]) => any>(constructor: T) {
+        //  把类的构造函数作为参数传入，并修改构造函数
+        return class extends constructor {
+            // 修改当前类的构造函数
+            name = 'zyt'
+            getName() {
+                return this.name;
+            }
+        }
+    }
+}
+
+@testDecorator2()
+class Test2 {
+    name:string
+    constructor(name:string) {
+        this.name = name
+    }
+    getData() {
+        return this.name
+    }
+}
+
+
+
+const test2 = new Test2('zyt1')
+console.log(test2.getData())
