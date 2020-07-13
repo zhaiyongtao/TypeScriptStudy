5494eaa311d393f9f6cd27c780be4a88b98c826a
zhaiyongtao <501671900@qq.com>
2020-07-13 18:25:45 +0800

    fix：增加文件类的装饰器2

--- 5_2_TypeScriptDecorator2/src/index.ts
+++ 5_2_TypeScriptDecorator2/src/index.ts
@@ -6,12 +6,10 @@
  * @Copyright: Copyright (c) 2020, Hand
  */
 
-// 装饰器工厂
-// 重载构造函数
-// 类装饰器表达式会在运行时当作函数被调用，类的构造函数作为其唯一的参数；
-// 如果类装饰器返回一个值，它会使用提供的构造函数来替换类的声明；
+
 
 function testDecorator() {
+    // T extends new (...args: any[]) => any  => T可以继承一个构造函数 => T可以通过他所继承的构造函数实例化出来  => T 是一个类
   return  function <T extends new (...args: any[]) => any>(constructor: T) {
       //  把类的构造函数作为参数传入，并修改构造函数
         return class extends constructor {
@@ -23,6 +21,8 @@ function testDecorator() {
         }
     }
 }
+
+// 这个时候testDecorator作为一个工厂函数使用
 const Test = testDecorator () (
     class  {
         name: string;
@@ -32,9 +32,6 @@ const Test = testDecorator () (
     }
 )
 
-const test = new Test('zyt')
-test.getName();
-console.log(test.getName())
 
 
 function testDecorator2() {
@@ -44,7 +41,7 @@ function testDecorator2() {
             // 修改当前类的构造函数
             name = 'zyt'
             getName() {
-                return this.name;
+                return this.name ;
             }
         }
     }
@@ -56,12 +53,11 @@ class Test2 {
     constructor(name:string) {
         this.name = name
     }
-    getData() {
-        return this.name
-    }
 }
 
 
 
-const test2 = new Test2('zyt1')
-console.log(test2.getData())
+const test2 = new Test2('zyt2')
+// 这个地方为什么要给test2 转换类型？
+// 这是Ts在检查代码的时候，发现类里并没有getName这个方法，这个方法是是我们在装饰器里加上的，所以这里不转换类型就会有语法报错
+console.log((test2 as any).getName())
